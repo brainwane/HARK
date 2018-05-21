5a143c12be718308c72d0c72545055f7ec28fa1c
Sumana Harihareswara <sh@changeset.nyc>
2018-05-21 17:01:29 -0400

    Update version, date, maintainer email, scipy dependency

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.8.0.dev3',  # Required
+    version='0.8.0.dev6',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -83,7 +83,7 @@ setup(
 
     # This should be a valid email address corresponding to the author listed
     # above.
-    author_email='ccarroll@llorracc.org',  # Optional
+    author_email='econ-ark@jhuecon.org',  # Optional
 
 
     license='Apache',
@@ -145,6 +145,7 @@ setup(
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=['numpy',
                       'matplotlib',
+                      'scipy',
                       'Sphinx',
                       'numpydoc',
                       'dill',
